4a5ea473e19ef2d3947013f11fe3dedfb468769e
Volodimir Moskaliuk <volodyko@volodyko-m-pc.zeo.lcl>
2017-08-14 11:39:10 +0300

    Initial Commit

---
+++ MVKQueue/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MVKQueue
+//
+//  Created by Volodimir Moskaliuk on 8/14/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
